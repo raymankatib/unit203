66933f5dd8b7f2a078ba3bccc21be220a34ee4a4
raymankatib <rayman.katib@gmail.com>
2021-12-02 13:22:59 -0500

    finished server

--- src/components/TotalComponent.js
+++ src/components/TotalComponent.js
@@ -1,9 +1,12 @@
-import { useEffect, useState } from 'react';
-import { Box, Heading, Flex } from '@chakra-ui/react';
+import { useEffect, useState, useRef } from 'react';
+import { Box, Heading, Flex, Input } from '@chakra-ui/react';
+import axios from 'axios';
 
-import { BLUE, SUBTOTAL, HST, ESTIMATED_DELIVERY, TOTAL } from '../config/variables';
+import { BLUE, ESTIMATED_DELIVERY } from '../config/variables';
 export default function TotalComponent({ items }) {
+	const postalRef = useRef();
 	const [fees, setFees] = useState({});
+	const [estimateDelivery, setEstimateDelivery] = useState('');
 	const calculateFees = () => {
 		const subtotal = items.reduce((acc, cur) => {
 			acc += cur.price;
@@ -20,11 +23,28 @@ export default function TotalComponent({ items }) {
 
 	useEffect(() => {
 		calculateFees();
-		console.log(fees);
 	}, [items]);
+
+	const handelSubmit = async (e) => {
+		e.preventDefault();
+		const userPostal = postalRef.current.value;
+		const { data: _estimateDelivery } = await axios.post('http://localhost:4001/postal-code', {
+			postalCode: userPostal,
+			itemIds: items.reduce((acc, cur) => {
+				acc = [...acc, cur.id];
+				return acc;
+			}, [])
+		});
+		setEstimateDelivery(_estimateDelivery);
+
+		postalRef.current.value = '';
+	};
 
 	return (
 		<Box>
+			<form onSubmit={(e) => handelSubmit(e)}>
+				<Input ref={postalRef} w="50%" type="text" placeholder="Enter your postal code (V, M, K)" />
+			</form>
 			<Flex justifyContent="space-between">
 				<Heading>Subtotal:</Heading>
 				<Heading>{fees.subtotal}$</Heading>
@@ -35,7 +55,7 @@ export default function TotalComponent({ items }) {
 			</Flex>
 			<Flex justifyContent="space-between">
 				<Heading>Shipping:</Heading>
-				<Heading>{ESTIMATED_DELIVERY}$</Heading>
+				<Heading>{estimateDelivery}</Heading>
 			</Flex>
 			<Flex justifyContent="space-between" color={BLUE}>
 				<Heading>Total:</Heading>
